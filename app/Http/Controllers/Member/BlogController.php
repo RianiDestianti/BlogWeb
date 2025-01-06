b0a3c3c0839a9bf7a785fbebaee9e2729833e78a
RianiDestianti <rianidestiantii@gmail.com>
2025-01-06 18:58:38 +0700

    store data baru

--- app/Http/Controllers/Member/BlogController.php
+++ app/Http/Controllers/Member/BlogController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Member;
+
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
@@ -15,11 +16,9 @@ class BlogController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('member.blogs.index',[
-        'Post'=> Post::latest()->paginate(3)
-    ]);
-        // $data = Post::where('user_id',$user->id)->orderBy('id','desc')->get();
-            
+        return view('member.blogs.index', [
+            'Post' => Post::latest()->paginate(3)
+        ]);
     }
 
     /**
@@ -35,7 +34,36 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'image|mimes:jpeg,jpg,png|max:10240'
+        ], [
+            'title.required' => 'Judul Wajib Diisi',
+            'content.required' => 'Konten Wajib Diisi',
+            'thumbnail.image' => 'Hanya Gambar Yang Diperbolehkan',
+            'thumbnail.mimes' => 'Ekstensi Yang Diperbolehkan Hanya JPEG, JPG, Dan PNG',
+            'thumbnail.max' => 'Ukuran Maksimum Untuk Thumbnail Adalah 10MB',
+        ]);
+
+        if ($request->hasFile('thumbnail')) {
+            $image = $request->file('thumbnail');
+            $image_name = time() . "_" . $image->getClientOriginalName();
+            $destination_path = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'));
+            $image->move($destination_path, $image_name);
+        };
+
+        $data = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'status' => $request->status,
+            'thumbnail' => isset($image_name) ? $image_name : null,
+            'slug' => $this->generateSlug($request->title),
+            'user_id' => Auth::user()->id
+        ];
+        Post::create($data);
+        return redirect()->route('member.blogs.index')->with('success', 'Data Berhasil Di-tambahkan');
     }
 
     /**
@@ -61,38 +89,38 @@ class BlogController extends Controller
     public function update(Request $request, Post $post)
     {
         $request->validate([
-            'title'=>'required',
-            'content'=>'required',
-            'thumbnail'=>'image|mimes:jpeg,jpg,png|max:10240'
-        ],[
-            'title.required'=>'Judul Wajib Diisi',
-            'content.required'=>'Konten Wajib Diisi',
-            'thumbnail.image'=>'Hanya Gambar Yang Diperbolehkan',
-            'thumbnail.mimes'=>'Ekstensi Yang Diperbolehkan Hanya JPEG, JPG, Dan PNG',
-            'thumbnail.max'=>'Ukuran Maksimum Untuk Thumbnail Adalah 10MB',
+            'title' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'image|mimes:jpeg,jpg,png|max:10240'
+        ], [
+            'title.required' => 'Judul Wajib Diisi',
+            'content.required' => 'Konten Wajib Diisi',
+            'thumbnail.image' => 'Hanya Gambar Yang Diperbolehkan',
+            'thumbnail.mimes' => 'Ekstensi Yang Diperbolehkan Hanya JPEG, JPG, Dan PNG',
+            'thumbnail.max' => 'Ukuran Maksimum Untuk Thumbnail Adalah 10MB',
         ]);
 
-        if($request->hasFile('thumbnail')){
-            if(isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'))."/".$post->thumbnail)){
-                unlink(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'))."/".$post->thumbnail);
+        if ($request->hasFile('thumbnail')) {
+            if (isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . "/" . $post->thumbnail)) {
+                unlink(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . "/" . $post->thumbnail);
             }
-            
+
             $image = $request->file('thumbnail');
-            $image_name = time()."_".$image->getClientOriginalName();
+            $image_name = time() . "_" . $image->getClientOriginalName();
             $destination_path = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'));
             $image->move($destination_path, $image_name);
         };
 
-        $data=[
-            'title'=>$request->title,
-            'description'=>$request->description,
-            'content'=>$request->content,
-            'status'=>$request->status,
-            'thumbnail'=>isset($image_name)?$image_name:$post->thumbnail,
-            'slug'=>$this->generateSlug($request->title,$post->id)
+        $data = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'content' => $request->content,
+            'status' => $request->status,
+            'thumbnail' => isset($image_name) ? $image_name : $post->thumbnail,
+            'slug' => $this->generateSlug($request->title, $post->id)
         ];
-        Post::where('id',$post->id)->update($data);
-        return redirect()->route('member.blogs.index')->with('success','Data Berhasil Di-Update');
+        Post::where('id', $post->id)->update($data);
+        return redirect()->route('member.blogs.index')->with('success', 'Data Berhasil Di-Update');
     }
 
     /**
@@ -102,18 +130,17 @@ class BlogController extends Controller
     {
         //
     }
-    private function generateSlug($title,$id){
+
+    private function generateSlug($title, $id = null)
+    {
         $slug = Str::slug($title);
-        $count = Post::Where('slug',$slug)->when($id, function($query,$id){
-            return $query->where('id','!=','$id');
+        $count = Post::where('slug', $slug)->when($id, function ($query, $id) {
+            return $query->where('id', '!=', $id);
         })->count();
 
-        if($count > 0){
-            $slug = $slug."-".($count+1);
-
-            
+        if ($count > 0) {
+            $slug = $slug . "-" . ($count + 1);
         }
         return $slug;
-
     }
 }
