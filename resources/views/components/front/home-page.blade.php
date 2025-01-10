88cd50b595c5164708a8b76492c3b0c80f9b2446
RianiDestianti <rianidestiantii@gmail.com>
2025-01-10 11:16:36 +0700

    daftar data home new

--- resources/views/components/front/home-page.blade.php
+++ resources/views/components/front/home-page.blade.php
@@ -1,27 +1,22 @@
-
 <x-front.layout>
     <!-- Main Content-->
     <div class="container px-4 px-lg-5">
-     <div class="row gx-4 gx-lg-5 justify-content-center">
-         <div class="col-md-10 col-lg-8 col-xl-7">
-            @foreach($Post as $item)
-            <!-- Post preview-->
-            <x-front.blog-list 
-                title="{{ $item->title }}"
-                description="{{ $item->description }}"
-                data="{{ $item->created_at->isoFormat('LL') }}"
-                user="{{ $item->user->name }}"
-                
-            />
-        @endforeach
-        <!-- Pager-->
-             <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
-         </div>
-     </div>
- </div>
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                @foreach($Post as $item)
+                <!-- Post preview-->
+                <x-front.blog-list 
+                    title="{{ $item->title }}"
+                    description="{{ $item->description }}"
+                    data="{{ $item->created_at->isoFormat('dddd, D MMMM Y') }}"
+                    user="{{ $item->user->name }}"
+                />
+                @endforeach
+                <!-- Pager-->
+                <div class="d-flex justify-content-end mb-4">
+                    <a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-front.layout>
- 
-      
-        
-  
- 
